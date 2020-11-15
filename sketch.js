4206ac79cfba064c6dbac79ffa11d9b7ed1f7d6a
SanjanaAvileli <70663967+SanjanaAvileli@users.noreply.github.com>
2020-11-14 19:19:02 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+
+
+
+function setup() {
+  createCanvas(480,800);
+
+    
+  
+    createSprite(30, 800, 15000, 50);
+
+  
+}
+
+function draw() {
+  background(255,255,255);  
+
+ 
+
+  drawSprites();
+}
